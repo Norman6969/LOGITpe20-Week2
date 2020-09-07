425c857d8b19e19c7ac996b04482d2ab2d6b97cf
Opilane TTHK <opilane@tthk.ee>
2020-09-07 14:34:52 +0300

    Commit

---
+++ Week2/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.VisualBasic;
+using System;
+
+namespace Week2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crate an app
+             * 1. App asks the user's name and greets the user by it's name;
+             * 2. App asks the user's age and calculates
+             * what year the user was born;
+             * 3. App displays the result to the user
+              
+             */
+            Console.WriteLine("what is your name?");
+            String Name;
+            Name = Console.ReadLine();
+            Console.WriteLine("Hello!" + Name);
+            Console.WriteLine("How old are you ?");
+            String UserInput;
+            UserInput = Console.ReadLine();
+            int Age;
+            Age = Int32.Parse(UserInput);
+            int Year = 2020 - Age;
+            Console.WriteLine("You were born in " + Year + ".");
+
+            
+
+        }
+    }
+}
